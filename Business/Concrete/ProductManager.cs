272d1893a5c569d699450088c63ec3388d58e016
Enes Usluer <eusluer.eu@gmail.com>
2022-07-25 01:24:02 +0300

    Entity Framework

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -19,9 +19,24 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public List<Category> GetAll()
+        public List<Product> GetAll()
         {
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<=max);
+        }
+
+        List<Product> IProductService.GetAll()
+        {
+            throw new NotImplementedException();
         }
     }
 }
